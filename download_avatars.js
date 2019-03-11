e1565f585a86bf17053ffd13ab1515b22642787c
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-11 21:27:04 +0000

    Added proper authorization for getrepo function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,24 @@
 var request = require('request');
+var gitToken = require('./secrets');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent' : 'request',
+      'Authorization' : 'gitToken'
+    }
+  }
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
+}
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
 
 console.log('Welcome to the Github Avatar Downloader!');

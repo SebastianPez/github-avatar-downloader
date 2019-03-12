03888a6f6f13bc670c1276049bfba2db34b16cc2
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-12 00:49:22 +0000

    avatar_url is now console logging the url for each user

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,34 @@
 var request = require('request');
-var gitToken = require('./secrets');
+var gitToken = require('./secrets').GITHUB_TOKEN;
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent' : 'request',
-      'Authorization' : 'gitToken'
+      'User-Agent' : 'SebastianPez',
+      'Authorization'  : 'gitToken'
     }
+  }
+  function JSONParse(data) {
+    var userInfo = JSON.parse(data);
+    return userInfo;
   }
 
   request(options, function(err, res, body) {
-    cb(err, body);
+    res.setEncoding('utf8');
+    cb(err, JSONParse(body));
   });
 }
+
+
 getRepoContributors("jquery", "jquery", function(err, result) {
+  for (let users of result) {
+    console.log(users.avatar_url);
+  }
   console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Result:", result);
 });
 
 

9446266633f7126d2929c6eb25f3decb4fe0b778
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-12 02:11:45 +0000

    worked through hardcoded downloadImageByURL with @kcdporter

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var gitToken = require('./secrets').GITHUB_TOKEN;
+var fs = require('fs');
 
 
 function getRepoContributors(repoOwner, repoName, cb) {
@@ -7,10 +8,11 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent' : 'SebastianPez',
-      'Authorization'  : 'gitToken'
+      'User-Agent': 'SebastianPez',
+      'Authorization': 'token ' + gitToken
     }
   }
+
   function JSONParse(data) {
     var userInfo = JSON.parse(data);
     return userInfo;
@@ -22,14 +24,29 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-
 getRepoContributors("jquery", "jquery", function(err, result) {
-  for (let users of result) {
-    console.log(users.avatar_url);
+  if (err) {
+    console.log("Errors: ", err);
+    return err;
   }
-  console.log("Errors:", err);
-  // console.log("Result:", result);
+  for (let user of result) {
+    downloadImageByURL(user.avatar_url, "./avatars/" + user.login + ".jpeg")
+  }
 });
+
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+    .on('error', function (err) {
+      console.log("Error code : " + err.statusCode)
+    })
+
+    .on('response', function (res) {
+      console.log('Image Info : StatCode: ' + res.statusCode + ', StatMessage' + res.statusMessage + ", ContentType: " + res.headers['content-type']);
+      console.log("Avatar download complete!")
+    })
+    .pipe(fs.createWriteStream(filePath))
+  }
 
 
 console.log('Welcome to the Github Avatar Downloader!');
